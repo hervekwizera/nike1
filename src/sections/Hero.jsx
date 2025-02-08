f647f112a76564298bbd65d175be3c4443a2bcdf
hervekwizera <hervekwizera63@gmail.com>
2025-02-08 19:30:28 +0200

    writting code in Hero section and add button in components folder

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,8 +1,35 @@
-
+import Button from "../components/Button";
+import { arrowRight } from "../assets/icons";
 function Hero() {
   return (
-    <div>Test</div>
-  )
+    <section
+      id="home"
+      className="w-full flex
+    xl:flex-row flex-col 
+    justify-center min-h-screen
+    gap-10 max-container"
+    >
+      <div
+        className="relative
+      xl:w-2/5 flex flex-col
+      justify-center items-start
+     w-full max-xl:padding-x pt-28 "
+      >
+        <p> Our summer collection</p>
+        <h1>
+          <span>The New Arrival</span>
+          <br />
+          <span>Nike shoes</span>
+          <p>
+            Discover stylish Nike arrivals, quality comfort, and innovation for
+            your active life.
+          </p>
+          <Button label="Shop now"
+          iconURL={arrowRight}/>
+        </h1>
+      </div>
+    </section>
+  );
 }
 
-export default Hero
+export default Hero;
