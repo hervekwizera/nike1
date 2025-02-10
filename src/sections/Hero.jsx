7aa307df2680637e1119f0afbc7ffa1df466d497
hervekwizera <hervekwizera63@gmail.com>
2025-02-10 19:19:52 +0200

    to day i styled Hero section

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,44 +1,43 @@
 import Button from "../components/Button";
 import { arrowRight } from "../assets/icons";
 import { statistics } from "../constants";
+
 function Hero() {
   return (
     <section
       id="home"
-      className="w-full flex
-    xl:flex-row flex-col 
-    justify-center min-h-screen
-    gap-10 max-container"
+      className="w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container"
     >
-      <div
-        className="relative
-      xl:w-2/5 flex flex-col
-      justify-center items-start
-     w-full max-xl:padding-x pt-28 "
-      >
-        <p> Our summer collection</p>
-        <h1>
-          <span>The New Arrival</span>
+      <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:px-6 pt-28">
+        <p className="text-xl font-montserrat text-coral-red">
+          Our summer collection
+        </p>
+
+        <h1 className="mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82px] font-bold">
+          <span className="xl:bg-white xl:whitespace-nowrap relative z-10 pr-10">
+            The New Arrival
+          </span>
           <br />
-          <span>Nike shoes</span>
-          <p>
-            Discover stylish Nike arrivals, quality comfort, and innovation for
-            your active life.
-          </p>
-          <Button label="Shop now" iconURL={arrowRight} />
-          <div
-            className="flex  justify-start 
-            flex-wrap w-full mt-20
-            gap-16"
-          >
-            {statistics.map((stat) => (
-              <div key={stat.label}>
-                <p>{stat.value}</p>
-                <p>{stat.label}</p>
-              </div>
-            ))}
-          </div>
+          <span className="text-coral-red inline-block mt-3">Nike shoes</span>
         </h1>
+
+        <p className="mt-5 text-lg text-gray-600 max-w-md">
+          Discover stylish Nike arrivals, quality comfort, and innovation for
+          your active life.
+        </p>
+
+        <div className="mt-6">
+          <Button label="Shop now" iconURL={arrowRight} />
+        </div>
+
+        <div className="flex justify-start flex-wrap w-full mt-20 gap-16">
+          {statistics.map((stat) => (
+            <div key={stat.label} className="text-center">
+              <p className="text-4xl font-bold text-gray-900">{stat.value}</p>
+              <p className="text-lg text-gray-600">{stat.label}</p>
+            </div>
+          ))}
+        </div>
       </div>
     </section>
   );
