e367f4de77a8dee40b3f08afc0c898028ac0ef4e
hervekwizera <hervekwizera63@gmail.com>
2025-02-20 11:19:26 +0200

    modifing code in Super Quality section

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -7,11 +7,11 @@
     bg-coral-red rounded-full text-white 
     border-coral-red">
     {label}
-    <img src={iconURL} 
+    {iconURL && <img src={iconURL} 
     alt="arrow right icon" 
     className="ml-2 rounded-full
     w-5 h-5 "
-    />
+    />}
     </button>
   )
 }
--- src/sections/SuperQuality.jsx
+++ src/sections/SuperQuality.jsx
@@ -6,24 +6,27 @@ const SuperQuality = () => {
       id="about-us"
       className="flex justify-between items-center max-lg:flex-col gap-10 w-full max-container" >
       <div className="flex flex-1 flex-col">
-        <p className="text-xl font-montserrat text-coral-red">
-          Our summer collection
-        </p>
-
-        <h2 className="mt-10 font-palanquin text-7xl max-sm:text-6xl max-sm:leading-tight font-bold">
-          <span className="xl:bg-white xl:whitespace-nowrap relative z-10 pr-10">
-            The New Arrival
-          </span>
-          <br />
-          <span className="text-coral-red inline-block mt-3">Nike Shoes</span>
+        <h2 className="font-palanquin text-4xl capitalize
+          font-bold lg:max-w-lg" >
+          We Provide You <span className="
+          text-coral-red">Super </span>
+           <span className="
+          text-coral-red">Quality</span>
+          Shoes
         </h2>
-
-        <p className="mt-5 text-lg text-gray-600 max-w-md">
-          Discover stylish Nike arrivals, quality comfort, and innovation for
-          your active life.
+        <p className="mt-4 lg:max-w-lg
+        info-text">
+         Ensuring premium comfort and
+         style , our meticulously crafted
+         footwear is deigned to elevate your 
+         experience, providing you with unmatched quality,
+         innovation, and a touch of elegance. 
         </p>
-
-        <div className="mt-6">
+        <p className="mt-6 lg:max-w-lg
+        info-text">Our dedication to 
+        detail and excellence ensures
+        your satisfaction</p>
+        <div className="mt-11">
           <Button label="View Details" />
         </div>
       </div>
