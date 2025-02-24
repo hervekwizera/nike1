a83dc13129c676613a438eb74052e0904409863d
hervekwizera <hervekwizera63@gmail.com>
2025-02-24 19:00:21 +0200

    adding some code in specialOffer.jsx

--- src/sections/SpecialOffer.jsx
+++ src/sections/SpecialOffer.jsx
@@ -1,4 +1,5 @@
 import {offer} from "../assets/images"
+import Button from "../components/Button";
 const SpecialOffer = () => {
   return (
     <section className="flex justify-wrap
@@ -8,6 +9,28 @@ const SpecialOffer = () => {
         height={687}
         className="object-contain
         w-full" />
+      </div>
+      <div className="flex flex-1 flex-col">
+        <h2 className="font-palanquin text-4xl capitalize
+          font-bold lg:max-w-lg" ><span className="
+          text-coral-red"> Special </span>
+           Offer
+        </h2>
+        <p className="mt-4 lg:max-w-lg
+        info-text">
+         Ensuring premium comfort and
+         style , our meticulously crafted
+         footwear is deigned to elevate your 
+         experience, providing you with unmatched quality,
+         innovation, and a touch of elegance. 
+        </p>
+        <p className="mt-6 lg:max-w-lg
+        info-text">Our dedication to 
+        detail and excellence ensures
+        your satisfaction</p>
+        <div className="mt-11">
+          <Button label="View Details" />
+        </div>
       </div>
     </section>
   )
